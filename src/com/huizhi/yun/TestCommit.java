72c3dc3bf04a7bd40c0af3d36669fbb305d5ce70
wdx <792973226@qq.com>
2019-09-04 10:29:34 +0800

    第四次提交测试冲突

--- src/com/huizhi/yun/TestCommit.java
+++ src/com/huizhi/yun/TestCommit.java
@@ -7,7 +7,7 @@ package com.huizhi.yun;
  */
 public class TestCommit {
 	public static void main(String[] args) {
-		System.out.println("第三次提交测试冲突");
+		System.out.println("第四次提交测试冲突");
 		System.out.println("????");
 	}
 }
